7cf243750788e0b555405d2f1aa50dd0b55012d9
NeilRow56 <neilrowland56@gmail.com>
2023-07-20 11:16:33 +0100

    Reset database and added ridirect to dashboard for newly created store. Updated dashboard-storeId-routes-page.tsx to show active store

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,40 @@
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+
+import prismadb from "@/lib/prismadb"
+
+export default async function DashboardLayout ({
+    children,
+    params,
+
+}: {
+    children: React.ReactNode
+    params: {storesId: string}
+}) {
+
+    const { userId } = auth()
+
+    if(!userId) {
+        redirect("/sign-in") 
+    }
+
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storesId,
+            userId: userId,
+        }
+    })
+
+    if (!store) {
+        redirect("/")
+    }
+
+    return (
+        <>
+        <div>
+            This will be a Navbar
+        </div>
+        { children}
+        </>
+    )
+}
