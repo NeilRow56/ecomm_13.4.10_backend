13dff11b686282ce53ce02b317a42e0ab0d63c69
NeilRow56 <neilrowland56@gmail.com>
2023-07-21 07:12:26 +0100

    Added store-switcher and updated navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,29 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 import { MainNav } from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
+import prismadb from "@/lib/prismadb";
 
 
-const Navbar = () => {
+const Navbar =  async () => {
+
+    const { userId } = auth()
+
+    if (!userId) {
+        redirect("/sign-in");
+      }
+
+      const stores = await prismadb.store.findMany({
+        where: {
+          userId: userId
+        },
+      });
+
  return (
    <div className="border-b ">
      <div className="flex h-16 items-center px-4">
-       <div>This will be The Store switcher</div>
+       <StoreSwitcher  items={stores}/>
        <div>
         <MainNav className="mx-6"/>
        </div>
